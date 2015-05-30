45c254a46940a3b06537bad8f45d42eca09a6cd5
Pablo Diaz-Gutierrez <ihaveajob@gmail.com>
2015-05-30 16:30:17 -0700

    Forgot to add the main file

---
+++ ispcheck.py
@@ -0,0 +1,18 @@
+# If the list grows large, it might be worth using a dictionary
+isps = ('gmail.', 'yahoo.', 'earthlink.', 'comcast.', 'att.', 'movistar.', 'hotmail.', 'mail.', 'googlemail.',
+        'msn.', 'bellsouth.', 'telus.', 'optusnet.', 'qq.', 'sky.', 'icloud.', 'mac.', 'sympatico.',
+        'xtra.', 'web.', 'cox.', 'ymail.', 'aim.', 'rogers.', 'verizon.', 'rocketmail.', 'google.', 'optonline.',
+        'sbcglobal.', 'aol.', 'me.', 'btinternet.', 'charter.', 'shaw.')
+
+def check_domain(domain):
+  'Return True if the domain is a known ISP; False otherwise.'
+  for isp in isps:
+    if domain.startswith(isp):
+      return True
+  return False
+
+def check_email(email):
+  'Return True if the domain for this email is a known ISP; False otherwise.'
+  domain = email.split('@')[1]
+  return check_domain(domain)
+
